fb7e374abd7c88f699dda48f36950818f622eae5
Marvin Banguero <marvin.banguero@infracommerce.lat>
2024-07-26 15:49:15 -0500

    Start & end for the cutting videos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, render_template, redirect, url_for, send_file, abort
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+import random
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
@@ -13,27 +13,24 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 def cortar_video(input_video_path, duracion_segmento, inicio_path=None, final_path=None):
     video = VideoFileClip(input_video_path)
     duracion_total = int(video.duration)
-    clips = []
+    segments = []
 
-    if inicio_path:
-        inicio_clip = VideoFileClip(inicio_path)
-        clips.append(inicio_clip)
+    inicio_clip = VideoFileClip(inicio_path) if inicio_path else None
+    final_clip = VideoFileClip(final_path) if final_path else None
 
     for start_time in range(0, duracion_total, duracion_segmento):
         end_time = min(start_time + duracion_segmento, duracion_total)
         clip = video.subclip(start_time, end_time)
-        clips.append(clip)
+        if inicio_clip:
+            clip = concatenate_videoclips([inicio_clip, clip])
+        if final_clip:
+            clip = concatenate_videoclips([clip, final_clip])
+        output_filename = f"segmento_{start_time}_{end_time}.mp4"
+        output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+        clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
+        segments.append(output_filename)
 
-    if final_path:
-        final_clip = VideoFileClip(final_path)
-        clips.append(final_clip)
-
-    video_final = concatenate_videoclips(clips)
-    output_filename = "video_segmentado.mp4"
-    output_path = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
-    video_final.write_videofile(output_path, codec='libx264', audio_codec='aac')
-
-    return output_filename
+    return segments
 
 def cortar_y_mezclar_video(input_video_path, duracion_segmento):
     video = VideoFileClip(input_video_path)
@@ -81,10 +78,10 @@ def index():
             final_path = os.path.join(app.config['UPLOAD_FOLDER'], final_file.filename)
             final_file.save(final_path)
 
-        segmented_video_filename = cortar_video(filepath, duration, inicio_path, final_path)
-        return render_template('index.html', video=segmented_video_filename)
+        segments = cortar_video(filepath, duration, inicio_path, final_path)
+        return render_template('index.html', segments=segments)
 
-    return render_template('index.html', video=None)
+    return render_template('index.html', segments=None)
 
 @app.route('/randomize', methods=['GET', 'POST'])
 def randomize():
