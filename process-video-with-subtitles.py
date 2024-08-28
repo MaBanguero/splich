ee6cd9fc9f55d5e738a6e5934d914af1c9d7e2b4
Marvin Banguero <marvin.banguero@infracommerce.lat>
2024-08-28 11:33:19 -0500

    One script to process video

--- process-video-with-subtitles.py
+++ process-video-with-subtitles.py
@@ -2,11 +2,10 @@ import os
 import time
 import json
 import datetime
+import uuid
 import boto3
 import cv2
-import uuid
-import moviepy.editor as mp
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import VideoFileClip
 from pysrt import open as open_srt
 
 # AWS clients
@@ -21,9 +20,19 @@ BACKGROUND_MUSIC_FOLDER = 'background-music'
 LOCAL_FOLDER = '/tmp'
 OUTPUT_FOLDER = 'reels'
 FRAGMENT_DURATION = 90  # Duration in seconds for each fragment
-FRAGMENT_LOG_FILE = 'processed_fragments.log'  # Log file to track processed fragments
-transcription_job_name = 'YourJobName'
 language_code = 'es-US'  # Modify if needed
+
+def upload_to_s3(local_path, s3_key):
+    # Ensure the file exists before attempting to upload
+    if not os.path.exists(local_path):
+        print(f"File {local_path} does not exist and cannot be uploaded.")
+        return
+
+    # Upload the file to S3
+    s3.upload_file(local_path, BUCKET_NAME, s3_key)
+    print(f"Uploaded {local_path} to s3://{BUCKET_NAME}/{s3_key}")
+
+
 
 def download_from_s3(s3_key, local_path):
     # Check if the file already exists
@@ -40,12 +49,10 @@ def download_from_s3(s3_key, local_path):
     s3.download_file(BUCKET_NAME, s3_key, local_path)
     print(f"Downloaded {s3_key} to {local_path}")
 
-
-
 def start_transcription_job(media_file_uri):
-    unique_job_name = f"{transcription_job_name}_{uuid.uuid4()}"
+    unique_job_name = f"transcription_{uuid.uuid4()}"
     try:
-        response = transcribe.start_transcription_job(
+        transcribe.start_transcription_job(
             TranscriptionJobName=unique_job_name,
             Media={'MediaFileUri': media_file_uri},
             MediaFormat='mp4',
@@ -53,12 +60,13 @@ def start_transcription_job(media_file_uri):
             OutputBucketName=BUCKET_NAME
         )
         print(f"Started transcription job: {unique_job_name}")
-        return response
+        return unique_job_name  # Return the job name as a string
     except Exception as e:
         print(f"Error starting transcription job: {str(e)}")
         raise
 
-def wait_for_job_completion():
+
+def wait_for_job_completion(transcription_job_name):
     while True:
         response = transcribe.get_transcription_job(TranscriptionJobName=transcription_job_name)
         status = response['TranscriptionJob']['TranscriptionJobStatus']
@@ -66,15 +74,15 @@ def wait_for_job_completion():
             if status == 'COMPLETED':
                 return response['TranscriptionJob']['Transcript']['TranscriptFileUri']
             else:
-                raise Exception("Transcription job failed")
-        time.sleep(30)
+                raise Exception(f"Transcription job {transcription_job_name} failed.")
+        time.sleep(10)
 
 def download_transcription(transcript_uri):
     transcript_file_name = os.path.join(LOCAL_FOLDER, 'transcription.json')
     s3.download_file(BUCKET_NAME, transcript_uri.split('/')[-1], transcript_file_name)
     return transcript_file_name
 
-def json_to_srt(json_file, srt_file):
+def json_to_srt(json_file, srt_file, start_time_offset=0):
     with open(json_file, 'r') as f:
         data = json.load(f)
 
@@ -82,8 +90,8 @@ def json_to_srt(json_file, srt_file):
         index = 1
         for item in data['results']['items']:
             if 'start_time' in item:
-                start_time = float(item['start_time'])
-                end_time = float(item['end_time'])
+                start_time = float(item['start_time']) + start_time_offset
+                end_time = float(item['end_time']) + start_time_offset
                 f.write(f"{index}\n")
                 f.write(f"{format_timestamp(start_time)} --> {format_timestamp(end_time)}\n")
                 f.write(f"{item['alternatives'][0]['content']}\n\n")
@@ -100,20 +108,6 @@ def format_timestamp(seconds):
 def get_text_size(text, font, font_scale, thickness):
     size = cv2.getTextSize(text, font, font_scale, thickness)[0]
     return size
-
-def split_text(text, max_width, font, font_scale, thickness):
-    words = text.split()
-    line1 = ""
-    line2 = ""
-    
-    for word in words:
-        if get_text_size(line1 + word, font, font_scale, thickness)[0] <= max_width:
-            line1 += word + " "
-        else:
-            if get_text_size(line2 + word, font, font_scale, thickness)[0] <= max_width:
-                line2 += word + " "
-    
-    return line1.strip(), line2.strip()
 
 def draw_background(frame, line1, line2, font, font_scale, thickness, elapsed_ratio, text_positions):
     words_line1 = line1.split()
@@ -139,104 +133,76 @@ def draw_background(frame, line1, line2, font, font_scale, thickness, elapsed_ra
     
     cv2.putText(frame, word, (x, y), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
 
-def add_subtitles(get_frame, t, subtitles):
-    frame = get_frame(t)
-    frame_cv2 = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+def split_text(text, max_width, font, font_scale, thickness):
+    words = text.split()
+    line1 = ""
+    line2 = ""
     
-    frame_height, frame_width, _ = frame_cv2.shape
-    margin = int(frame_width * 0.1)
-    safe_width = frame_width - 2 * margin
-    elapsed_ratio = (t % 2) / 2
+    for word in words:
+        if get_text_size(line1 + word, font, font_scale, thickness)[0] <= max_width:
+            line1 += word + " "
+        else:
+            if get_text_size(line2 + word, font, font_scale, thickness)[0] <= max_width:
+                line2 += word + " "
+    
+    return line1.strip(), line2.strip()
 
-    for subtitle in subtitles:
-        if subtitle.start.ordinal <= t*1000 < subtitle.end.ordinal:
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 1.6
-            thickness = 3
 
-            line1, line2 = split_text(subtitle.text, safe_width, font, font_scale, thickness)
-
-            text_size_line1 = get_text_size(line1, font, font_scale, thickness)
-            text_size_line2 = get_text_size(line2, font, font_scale, thickness)
-            text_x_line1 = (frame_width - text_size_line1[0]) // 2
-            text_x_line2 = (frame_width - text_size_line2[0]) // 2
-            text_y_line1 = (frame_height + text_size_line1[1]) // 2 - 30
-            text_y_line2 = (frame_height + text_size_line1[1]) // 2 + 20
-
-            text_positions = [(text_x_line1, text_y_line1), (text_x_line2, text_y_line2)]
-            draw_background(frame_cv2, line1, line2, font, font_scale, thickness, elapsed_ratio, text_positions)
-            
-            x = text_x_line1
-            for word in line1.split():
-                cv2.putText(frame_cv2, word, (x, text_y_line1), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
-                x += get_text_size(word, font, font_scale, thickness)[0] + 20
-
-            x = text_x_line2
-            for word in line2.split():
-                cv2.putText(frame_cv2, word, (x, text_y_line2), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
-                x += get_text_size(word, font, font_scale, thickness)[0] + 20
-            
-            break
-
-    return cv2.cvtColor(frame_cv2, cv2.COLOR_BGR2RGB)
-
-def process_video(video_filename, audio_filename, music_filename, srt_filename):
-    local_video_path = os.path.join(LOCAL_FOLDER, video_filename)
+def process_video(fragment_filename, audio_filename, music_filename, srt_filename):
+    local_fragment_path = os.path.join(LOCAL_FOLDER, fragment_filename)
     local_audio_path = os.path.join(LOCAL_FOLDER, audio_filename)
     local_music_path = os.path.join(LOCAL_FOLDER, music_filename)
 
-    # Process video in fragments
-    video_clip = VideoFileClip(local_video_path)
-    audio_clip = mp.AudioFileClip(local_audio_path)
-    music_clip = mp.AudioFileClip(local_music_path).volumex(0.25)  # Reduce music volume to 25%
-
+    video_clip = VideoFileClip(local_fragment_path)
     subtitles = open_srt(srt_filename)
 
-    for i in range(0, int(video_clip.duration), FRAGMENT_DURATION):
-        start_time = i
-        end_time = min(i + FRAGMENT_DURATION, video_clip.duration)
-        video_fragment = video_clip.subclip(start_time, end_time)
-        voice_fragment = audio_clip.subclip(start_time, end_time)
-        music_fragment = music_clip.subclip(0, min(90, video_fragment.duration))
+    def add_subtitles(get_frame, t):
+        frame = get_frame(t)
+        frame_cv2 = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        
+        frame_height, frame_width, _ = frame_cv2.shape
+        margin = int(frame_width * 0.1)
+        safe_width = frame_width - 2 * margin
 
-        combined_audio = mp.CompositeAudioClip([voice_fragment, music_fragment])
-        final_video_fragment = video_fragment.set_audio(combined_audio)
-        video_with_subtitles = final_video_fragment.fl(lambda gf, t: add_subtitles(gf, t, subtitles))
+        for subtitle in subtitles:
+            if subtitle.start.ordinal <= t*1000 < subtitle.end.ordinal:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = 1.6
+                thickness = 3
 
-        fragment_filename = f"fragment_{i // FRAGMENT_DURATION}_{video_filename}"
-        fragment_path = os.path.join(LOCAL_FOLDER, fragment_filename)
-        video_with_subtitles.write_videofile(fragment_path, codec='libx264', audio_codec='aac')
+                line1, line2 = split_text(subtitle.text, safe_width, font, font_scale, thickness)
 
-        upload_to_s3(fragment_path, f"{OUTPUT_FOLDER}/{fragment_filename}")
-        os.remove(fragment_path)
+                text_size_line1 = get_text_size(line1, font, font_scale, thickness)
+                text_size_line2 = get_text_size(line2, font, font_scale, thickness)
+                text_x_line1 = (frame_width - text_size_line1[0]) // 2
+                text_x_line2 = (frame_width - text_size_line2[0]) // 2
+                text_y_line1 = (frame_height + text_size_line1[1]) // 2 - 30
+                text_y_line2 = (frame_height + text_size_line1[1]) // 2 + 20
 
-def upload_to_s3(local_path, s3_key):
-    s3.upload_file(local_path, BUCKET_NAME, s3_key)
-    print(f"Uploaded {local_path} to {s3_key}")
-    
-def load_processed_fragments():
-    processed_fragments = {}
-    if not os.path.exists(FRAGMENT_LOG_FILE):
-        return processed_fragments
-    with open(FRAGMENT_LOG_FILE, 'r') as log_file:
-        for line in log_file:
-            parts = line.strip().split(',')
-            if len(parts) < 3:
-                print(f"Línea malformada en el archivo de log: {line}")
-                continue
-            video_filename = parts[0]
-            last_fragment = int(parts[1])
-            complete = parts[2].strip() == 'complete'
-            processed_fragments[video_filename] = {
-                'last_fragment': last_fragment,
-                'complete': complete
-            }
-    return processed_fragments
-    
-def save_processed_fragment(video_filename, fragment_index, complete=False):
-    with open(FRAGMENT_LOG_FILE, 'a') as log_file:
-        status = 'complete' if complete else 'incomplete'
-        log_file.write(f"{video_filename},{fragment_index},{status}\n")
+                text_positions = [(text_x_line1, text_y_line1), (text_x_line2, text_y_line2)]
+                draw_background(frame_cv2, line1, line2, font, font_scale, thickness, t / video_clip.duration, text_positions)
+                
+                x = text_x_line1
+                for word in line1.split():
+                    cv2.putText(frame_cv2, word, (x, text_y_line1), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
+                    x += get_text_size(word, font, font_scale, thickness)[0] + 20
+
+                x = text_x_line2
+                for word in line2.split():
+                    cv2.putText(frame_cv2, word, (x, text_y_line2), font, font_scale, (255, 255, 255), thickness, cv2.LINE_AA)
+                    x += get_text_size(word, font, font_scale, thickness)[0] + 20
+                
+                break
+
+        return cv2.cvtColor(frame_cv2, cv2.COLOR_BGR2RGB)
+
+    video_with_subtitles = video_clip.fl(add_subtitles)
+    output_path = os.path.join(LOCAL_FOLDER, f"output_{fragment_filename}")
+    video_with_subtitles.write_videofile(output_path, codec='libx264', audio_codec='aac')
+
+    upload_to_s3(output_path, f"{OUTPUT_FOLDER}/output_{fragment_filename}")
+    os.remove(output_path)
 
 def main():
     s3_video_objects = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=VIDEO_FOLDER).get('Contents', [])
@@ -249,9 +215,7 @@ def main():
 
     if not video_files or not audio_files or not music_files:
         print("No se encontraron archivos de video, audio o música de fondo.")
-        return
-
-    processed_fragments = load_processed_fragments()
+        return 
 
     for video_s3_key in video_files:
         video_filename = os.path.basename(video_s3_key)
@@ -260,10 +224,6 @@ def main():
 
         audio_filename = os.path.basename(audio_s3_key)
         music_filename = os.path.basename(music_s3_key)
-
-        if video_filename in processed_fragments and processed_fragments[video_filename]['complete']:
-            print(f"El video {video_filename} ya ha sido procesado completamente. Saltando...")
-            continue
 
         local_video_path = os.path.join(LOCAL_FOLDER, video_filename)
         local_audio_path = os.path.join(LOCAL_FOLDER, audio_filename)
@@ -288,17 +248,15 @@ def main():
             upload_to_s3(fragment_path, reel_s3_key)
 
             reel_media_uri = f"s3://{BUCKET_NAME}/{reel_s3_key}"
-            start_transcription_job(reel_media_uri)
+            transcription_job_name = start_transcription_job(reel_media_uri)
 
-            transcript_uri = wait_for_job_completion()
+            transcript_uri = wait_for_job_completion(transcription_job_name)
             transcript_file = download_transcription(transcript_uri)
 
             srt_filename = os.path.join(LOCAL_FOLDER, f"{fragment_filename}.srt")
-            json_to_srt(transcript_file, srt_filename)
+            json_to_srt(transcript_file, srt_filename, start_time_offset=start_time)
 
             process_video(fragment_filename, audio_filename, music_filename, srt_filename)
-
-            save_processed_fragment(video_filename, fragment_index, complete=True)
 
             os.remove(fragment_path)
             os.remove(transcript_file)
@@ -312,3 +270,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+if __name__ == "__main__":
+    main()
+
+    
