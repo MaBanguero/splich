0773083b4847a2fb8658f322da99d5e038343d4c
Marvin Banguero <marvin.banguero@infracommerce.lat>
2024-08-28 23:30:12 -0500

    Crear reels

--- crear-reels/video_processing.py
+++ crear-reels/video_processing.py
@@ -37,16 +37,16 @@ def process_single_reel(video_path, video_filename, start_time, fragment_index, 
     voice_clip = AudioFileClip(local_voice_path)
     voice_clip = normalize_audio(voice_clip)
 
-    # Asegurarse de que el nombre del archivo de salida sea correcto
-    audio_s3_key = f"{OUTPUT_FOLDER}/voice_fragment_{fragment_index}_{video_filename}.wav"
-    complete_audio_path = os.path.join(LOCAL_FOLDER, f"complete_{audio_s3_key}")
+    # Asegurarse de que el nombre del archivo de salida sea correcto y evitar doble extensión
+    audio_s3_key = f"voice_fragment_{fragment_index}_{video_filename}.wav"
+    complete_audio_path = os.path.join(LOCAL_FOLDER, audio_s3_key)
 
     # Escribir el archivo de audio para transcripción
     try:
         voice_clip.write_audiofile(complete_audio_path)
     except OSError as e:
         logger.error(f"Error writing audio file: {e}")
-        return
+        return None, None
 
     # Subir el archivo de audio a S3
     upload_to_s3(complete_audio_path, audio_s3_key)
